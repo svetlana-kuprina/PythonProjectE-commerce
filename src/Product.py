b3e5790763d9f9061f7ca1a50b979062439d420d
svetlana-kuprina <kupsveta@rambler.ru>
2025-11-23 13:21:36 +0500

    ДЗ 14.1

--- src/Product.py
+++ src/Product.py
@@ -9,5 +9,24 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, name: str, description: str, price: float, quantity: int):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """ Геттер приватного атрибута цена"""
+
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        """ Сеттер приватного атрибута цена с проверкой цены на 0 и отрицательность"""
+
+        if value <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        else:
+            self.__price = value
